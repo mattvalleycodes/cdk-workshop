28ad00c926371df9d20427b8389b40f6abf756b3
mattvalleycodes <hi+mattvalleycodes@mv.id.au>
2023-09-26 14:17:10 +1000

    feat: print endpoint urls

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,4 +1,4 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Duration, Stack, StackProps } from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import { Construct } from "constructs";
@@ -6,6 +6,9 @@ import { TableViewer } from "cdk-dynamo-table-viewer";
 import { HitCounter } from "./hitcounter";
 
 export class CdkWorkshopStack extends Stack {
+  public readonly hcViewerUrl: CfnOutput;
+  public readonly hcEndpoint: CfnOutput;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -19,14 +22,22 @@ export class CdkWorkshopStack extends Stack {
       downstream: hello,
     });
 
-    new apigw.LambdaRestApi(this, "Endpoint", {
+    const gateway = new apigw.LambdaRestApi(this, "Endpoint", {
       handler: helloWithCounter.handler,
     });
 
-    new TableViewer(this, "ViewHitCounter", {
+    const tv = new TableViewer(this, "ViewHitCounter", {
       title: "Hello Hits",
       table: helloWithCounter.table,
       sortBy: "hits",
+    });
+
+    this.hcEndpoint = new CfnOutput(this, "GatewayUrl", {
+      value: gateway.url,
+    });
+
+    this.hcViewerUrl = new CfnOutput(this, "TableViewerUrl", {
+      value: tv.endpoint,
     });
   }
 }
